192b6ca84b3b36ca12c28cc57ca762f9eba772d7
JonathanHdzDev <jonathan32hdz@live.com>
2024-07-09 10:00:43 -0600

    Ejemplo de Instanciar

---
+++ Instancias/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instancias
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductosID productosID2 = new ProductosID();  //Crea la instancia "ProductosID" y la convierte en objeto "productosID2();" 
+
+            productosID2.ID = " A ";                          //productosID2.ID esta llamando lo que tenga el ID "A"
+
+            productosID2.name = " B ";
+
+            Console.WriteLine(productosID2.name + productosID2.ID);
+            Console.ReadKey();
+        }
+    }
+}
