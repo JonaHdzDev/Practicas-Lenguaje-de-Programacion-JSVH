36b359895a526e1925696d44fd1e3444557aec10
JonathanHdzDev <jonathan32hdz@live.com>
2024-07-09 09:17:05 -0600

    Practica ContarCaracteres

---
+++ ContarCaracteres/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ContarCaracteres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un texto: ");
+            string texto = Console.ReadLine();
+            int contador = ContarCaracteres(texto);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("La cadena " + texto + " " + " contiene " + contador + " caracteres " );
+            Console.ReadKey();
+        }
+
+        static int ContarCaracteres(string cadena)
+        {
+            return cadena.Length;
+        }
+
+    }
+}
